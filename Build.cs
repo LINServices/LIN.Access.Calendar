0877c7ddbc9849cb699ba056f38761df05a75118
Alex <giraldojhong4@gmail.com>
2024-06-21 06:20:42 -0500

    Mejoras

--- Build.cs
+++ Build.cs
@@ -15,7 +15,7 @@ public class Build
     public static void Init(string? url = null)
     {
         Service._Service = new();
-        Service._Service.SetDefault(url ?? "http://api.calendar.linapps.co/");
+        Service._Service.SetDefault(url ?? "http://api.calendar.linplatform.com/");
         //Service._Service.SetDefault(url ?? "http://localhost:5253/");
     }
 
